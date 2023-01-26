fbcc6765a2bb8b16ccca60b15972ecf175e4ecd0
Damian Gawronski <damiangaw@gmail.com>
2023-01-26 14:02:13 +0100

    Add BottomNavigationBar to HomePage

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({
     Key? key,
     required this.user,
@@ -10,23 +10,65 @@ class HomePage extends StatelessWidget {
   final User user;
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
+    if (currentIndex == 0) {
+      return Center(
+        child: Text('jeden'),
+      );
+    }
+    if (currentIndex == 1) {
+      return Center(
+        child: Text('dwa'),
+      );
+    }
+
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('jestes zalogowany jako ${user.email}'),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                },
-                child: const Text('wyloguj'))
-          ],
-        ),
+      body: Builder(builder: (context) {
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('jestes zalogowany jako ${widget.user.email}'),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut();
+                  },
+                  child: const Text('wyloguj'))
+            ],
+          ),
+        );
+      }),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (newIndex) {
+          setState(() {
+            currentIndex = newIndex;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.reviews),
+            label: 'Opinie',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Dodaj',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Moje konto',
+          ),
+        ],
       ),
     );
   }
