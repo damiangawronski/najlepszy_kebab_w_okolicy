95607db89a3f2f3d0021012c46acb618dc1f1be0
Damian Gawronski <damiangaw@gmail.com>
2023-01-28 14:05:28 +0100

    Implement adding places

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -1,14 +1,55 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AddOpinionPageContent extends StatelessWidget {
+class AddOpinionPageContent extends StatefulWidget {
   const AddOpinionPageContent({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<AddOpinionPageContent> createState() => _AddOpinionPageContentState();
+}
+
+class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
+  var restaurantName = '';
+  var kebabName = '';
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('dwa'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'podaj nazwę lokalu',
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                restaurantName = newValue;
+              });
+            },
+          ),
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'podaj nazwę kebaba',
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                kebabName = newValue;
+              });
+            },
+          ),
+          ElevatedButton(
+              onPressed: () {
+                FirebaseFirestore.instance.collection('places').add({
+                  'name': restaurantName,
+                  'kebab': kebabName,
+                  'rating': 3.0,
+                });
+              },
+              child: const Text('dodaj'))
+        ],
+      ),
     );
   }
 }
