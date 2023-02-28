2595d90c4ae750216e575bafc405e56068c68bbe
Damian Gawronski <damiangaw@gmail.com>
2023-02-28 19:17:49 +0100

    Add restaurants cubit

---
+++ lib/app/home/places/cubit/places_cubit.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+import 'package:meta/meta.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'places_state.dart';
+
+class PlacesCubit extends Cubit<PlacesState> {
+  PlacesCubit()
+      : super(
+          const PlacesState(
+            documents: [],
+            errorMessage: '',
+            isLoading: false,
+          ),
+        );
+
+  StreamSubscription? _streamSubscription;
+
+  Future<void> start() async {
+    emit(
+      const PlacesState(
+        documents: [],
+        errorMessage: '',
+        isLoading: true,
+      ),
+    );
+
+    _streamSubscription = FirebaseFirestore.instance
+        .collection('places')
+        .orderBy('rating', descending: true)
+        .snapshots()
+        .listen((data) {
+      emit(
+        PlacesState(
+          documents: data.docs,
+          isLoading: false,
+          errorMessage: '',
+        ),
+      );
+    })
+      ..onError((error) {
+        emit(
+          PlacesState(
+            documents: const [],
+            isLoading: false,
+            errorMessage: error..toString(),
+          ),
+        );
+      });
+  }
+
+  @override
+  Future<void> close() {
+    _streamSubscription?.cancel();
+    return super.close();
+  }
+}
